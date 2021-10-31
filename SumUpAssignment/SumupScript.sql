9d84313d7ebce1e587adcf2a7b1a3e4dbc6ed5d5
ddimitrova1527 <79272276+ddimitrova1527@users.noreply.github.com>
2021-10-31 17:55:27 +0200

    Add files via upload

---
+++ SumUpAssignment/SumupScript.sql
@@ -0,0 +1,16 @@
+-- All customers from United States;
+Select * from customers 
+Where Country = ‘UnitedStates‘;
+
+-- All customers whose details were not updated from their creation on;
+Select * from customer_details 
+Where Created_at = Updated_at;
+
+-- The average customer age per country;
+Select AVG(customer_details.Age), customers.Country
+From customer_details
+INNER JOIN customers
+ON customer_details.ID=customers.ID
+Group by customers.Country;
+
+
